d1bb4f6c74b37406e828c751122e5441e9a35639
Anthony Chan <achan437@my.bcit.ca>
2021-03-05 13:31:38 -0800

    add input fields

--- nodes-to-myself/src/components/Login/index.js
+++ nodes-to-myself/src/components/Login/index.js
@@ -1,5 +1,5 @@
 import React, {useState,useEffect} from 'react'
-import { Card,CardHeader,CardContent,Tabs,Typography,Button } from '@material-ui/core';
+import { Card,CardHeader,CardContent,Tabs,Typography,Button,TextField } from '@material-ui/core';
 import { Close } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -23,8 +23,34 @@ export default function Login(){
             <Card className={classes.root}>
                 <CardHeader className={classes.header} title="Login"></CardHeader>
                 <CardContent>
-                    <Typography>USERNAME</Typography>
-                    <Button>Submit</Button>
+                    <form>
+                        <TextField
+                            value = {username}
+                            onChange = {e => setUsername(e.target.value)}
+                            fullWidth
+                            label="Username"
+                            variant="filled"
+                        >
+                        </TextField>
+                        <TextField
+                            value = {email}
+                            onChange = {e => setEmail(e.target.value)}
+                            fullWidth
+                            label="Email"
+                            variant="filled"
+                        >
+                        </TextField>
+                        <TextField
+                            type = "password"
+                            value = {password}
+                            onChange = {e => setPassword(e.target.value)}
+                            fullWidth
+                            label="Password"
+                            variant="filled"
+                        >
+                        </TextField>
+                        <Button>Submit</Button>
+                    </form>
                 </CardContent>
             </Card>
         </div>
