0d7b12ceb2277074c1d92fc2fafb2c926ed7cf2e
Anthony Chan <achan437@my.bcit.ca>
2021-03-05 15:33:04 -0800

    layout complete #8

--- nodes-to-myself/src/App.js
+++ nodes-to-myself/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './components/Login';
+import UserPage from './components/UserProfilePage'
 
 function App() {
   return (
     <div className="App">
-      
+      <UserPage></UserPage>
     </div>
   );
 }
