a88fbb1f1677f18bbd0e7ee2dfcc661f0ab9d8ef
Anthony Chan <achan437@my.bcit.ca>
2021-03-05 14:01:15 -0800

    remove unused import

--- nodes-to-myself/src/components/Login/index.js
+++ nodes-to-myself/src/components/Login/index.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import { Button,Card,CardHeader,CardContent,Tabs,Tab,TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles'
-import { blue } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
     root: {
