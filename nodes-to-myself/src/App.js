5461b448a953c56d5897d7d8e97092e99f75ca83
Kirill Kutnenko <kkutnenko@my.bcit.ca>
2021-03-08 00:40:31 -0800

    Logo file added, some styles added

--- nodes-to-myself/src/App.js
+++ nodes-to-myself/src/App.js
@@ -1,11 +1,14 @@
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header></Header>
+      <main style = {{marginTop: 100}}></main>
+    </Router>
   );
 }
 
