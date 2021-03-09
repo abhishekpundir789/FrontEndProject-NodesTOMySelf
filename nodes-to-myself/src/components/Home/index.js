e9f707543f16abb19bd1ce0727b2f57e6e28e14d
abhishekpundir789 <apundir@algomau.ca>
2021-03-09 11:59:54 -0800

    update commit with the clickable link on cards for home page

--- nodes-to-myself/src/components/Home/index.js
+++ nodes-to-myself/src/components/Home/index.js
@@ -1,31 +1,37 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import { HomeContainer, HomeH1, HomeWrapper, HomeCard, HomeH2, HomeP, HomeHeader} from './HomeElements'
-
-const pages = ["/todos", "/notes", "/images", "/links"]
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-    const [value, setValue] = React.useState()
     return (
         <HomeContainer>
             <HomeH1>Welcome to MyNodes!!!</HomeH1>
             <HomeHeader>Select your tasks for today</HomeHeader>
             <HomeWrapper>
+                <Link to = '/todos'>
                 <HomeCard>
                     <HomeH2>ToDo List</HomeH2>
                     <HomeP>Create your daily todo's here</HomeP>
                 </HomeCard>
+                </Link>
+                <Link to = '/notes'>
                 <HomeCard>
                     <HomeH2>Notes</HomeH2>
                     <HomeP>Make your important notes here</HomeP>
                 </HomeCard>
+                </Link>
+                <Link to = '/images'>
                 <HomeCard>
                     <HomeH2>Images</HomeH2>
                     <HomeP>Collect your images here</HomeP>
                 </HomeCard>
+                </Link>
+                <Link to = '/links'>
                 <HomeCard>
                     <HomeH2>Links</HomeH2>
                     <HomeP>Store your important links here</HomeP>
                 </HomeCard>
+                </Link>
             </HomeWrapper>
         </HomeContainer>
     )
