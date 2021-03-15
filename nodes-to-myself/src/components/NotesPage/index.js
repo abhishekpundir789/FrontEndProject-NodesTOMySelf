1ae86e695f9bff6540a6c6c5688e4a806d3afb14
abhishekpundir789 <apundir@algomau.ca>
2021-03-15 10:38:22 -0700

    uptate commit with notes page data

--- nodes-to-myself/src/components/NotesPage/index.js
+++ nodes-to-myself/src/components/NotesPage/index.js
@@ -2,3 +2,10 @@ import React from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+
+const Notes = props => props.data.map(note => <div>{note.text}</div>);
+
+export default () => {
+const data = [{text: 'Hey'}, {text: 'there'}];
+return <Notes data={data} />;
+}
