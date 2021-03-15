afdc52c351c0d87ae9ef6c741c348a57ccab1d24
Anthony Chan <achan437@my.bcit.ca>
2021-03-15 13:56:55 -0700

    dynamically displaying categories

--- nodes-to-myself/src/components/LinksPage/index.js
+++ nodes-to-myself/src/components/LinksPage/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import {Card, CardActions,Grid, CardContent, Button, Typography, TextField, IconButton} from '@material-ui/core';
+import {Card, CardHeader, Grid, CardContent, Button, Typography, TextField, IconButton} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
 
@@ -62,7 +62,7 @@ export default function LinksPage() {
         var exist = false
         categories.forEach(cat => {if(cat === newCategory){
             console.log(`${newCategory} already exist`)
-            exist = true;
+            exist = true
         }})
         if(!exist){
             setCategories([...categories, newCategory])
@@ -70,41 +70,52 @@ export default function LinksPage() {
         }
         setNewCategory("")
     }
-    
-    return (
-        <Grid container spacing ={3} className={classes.root}>            
-            <Button className={classes.button} variant="contained" color="primary" href="#contained-buttons">
-                New List
-            </Button>
-            {/* foreach loop to render list */}
-            <Card className={classes.card} variant="outlined">
-                <CardContent>
-                <form className={classes.header} noValidate autoComplete="off" onSubmit={addtoCategories}>            
-                    <TextField value={newCategory} id="basic" label="List Name" variant="outlined" onChange={(e) => {setNewCategory(e.target.value)}}/>
-                    <Button type="submit"> Save List Name </Button>
-                </form>                
-                    <Typography className={classes.pos} color="textSecondary">                
-                        <form noValidate autoComplete="off"> 
-                
-                            <TextField id="standard-basic" label="Links"/>
-                            <IconButton aria-label="delete" className={classes.margin}>
-                                <DeleteIcon />
-                            </IconButton>
-                        </form>
-                        <div>
-                            <AddCircleRoundedIcon style={{fill: "#4054b4"}}/>                
-                        </div>
-                    </Typography>           
-                </CardContent>
-                <CardActions>
-                    <div className={classes.buttonDiv}> 
-                        <Button variant="contained" color="secondary">
-                            Delete
-                        </Button>
-                    </div>
-                </CardActions>
-            </Card>
 
-        </Grid>
-      );
+    const removeCategory = (catName) => {
+        var tempCatergories = categories
+        for(var i = 0; i < tempCatergories.length; i++){
+            if(tempCatergories[i] === catName){
+                tempCatergories.splice(i,1)
+                setCategories([...tempCatergories])
+            }
+        }
+
     }
+
+    return (
+    <Grid container spacing ={3} className={classes.root}> 
+        <form className={classes.header} noValidate autoComplete="off" onSubmit={addtoCategories}>            
+            <TextField value={newCategory} id="basic" label="List Name" variant="outlined" onChange={(e) => {setNewCategory(e.target.value)}}/>
+            <Button type="submit">Add List</Button>
+        </form>
+        {categories.map(cat => {
+                return (
+                    <Card className={classes.card} variant="outlined">
+                        <CardHeader  title={cat}/>
+                        <CardContent>
+                            <Typography className={classes.pos} color="textSecondary">                
+                                <form noValidate autoComplete="off" > 
+                                    <TextField id="standard-basic" label="Links"/>
+                                    <IconButton aria-label="delete" className={classes.margin} type="submit">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </form>
+                                <div>
+                                    <AddCircleRoundedIcon style={{fill: "#4054b4"}}/>                
+                                </div>
+                            </Typography>           
+                        </CardContent>
+                        <CardContent>
+                            <div className={classes.buttonDiv}> 
+                                <Button variant="contained" color="secondary">
+                                    Delete Category
+                                </Button>
+                            </div>
+                        </CardContent>
+                    </Card>
+                )
+            })}
+    </Grid>
+        
+    );
+}
