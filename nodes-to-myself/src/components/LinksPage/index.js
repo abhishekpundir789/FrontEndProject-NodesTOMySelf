a25c5200de10cc8341321773463a267cbc250d07
Anthony Chan <achan437@my.bcit.ca>
2021-03-28 23:01:17 -0700

    front end of adding link in

--- nodes-to-myself/src/components/LinksPage/index.js
+++ nodes-to-myself/src/components/LinksPage/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import {Card, CardHeader, Grid, CardContent, Button, TextField, IconButton,Popover} from '@material-ui/core';
+import {Card, CardHeader, Grid, CardContent, Button, Typography, TextField, IconButton,Popover} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
 import * as _ from 'lodash'
@@ -67,6 +67,10 @@ const useStyles = makeStyles({
         margin: 5,
         marginTop: 10,
     },
+    newListButton:{
+        marginRight: "100%",
+
+    },
   });
 
 export default function LinksPage() {
@@ -74,12 +78,17 @@ export default function LinksPage() {
     const open = Boolean(newListAnchor);
     const newList = open ? 'simple-popover' : undefined;
 
+    const [newItemAnchor, setnewItemAnchor] = useState(false)
+
     const API_INVOKE_URL = 'https://tv45w0cj0b.execute-api.us-west-1.amazonaws.com/prod2'
     const classes = useStyles();
 
     const [linkLists, setlinkLists] = useState([])
-    const [linkList, setlinkList] = useState({id: "", description: "no title", category: "links", items: []})
+    const [linkList, setlinkList] = useState({id:"", description: "", category: "links", items: []})
+    const [currentId, setCurrentId] = useState("")
     const [listName, setlistName] = useState("")
+    const [itemsList, setitemsList] = useState([])
+    const [newItem, setNewItem] = useState("");
 
 
     const searchApi = async () =>{
@@ -112,12 +121,6 @@ export default function LinksPage() {
         setlistName("")
     }
 
-    const changingListName = async (e, list, id) => {
-        e.preventDefault()
-        updatelinkList(e, id)
-        setlistName("")
-    }
-
     const updatelinkList = async (e, id) => {
         e.preventDefault()
         fetch(API_INVOKE_URL + `/${id}`,{
@@ -126,6 +129,16 @@ export default function LinksPage() {
             headers: {'Content-Type': 'application/json'}
         })
             .then(() => {searchApi()})
+        setlistName("")
+    }    
+
+    const updateItems = async (e,list,id) => {
+        e.preventDefault()
+        var tempList = list.items
+        tempList.push(newItem)
+        setitemsList(tempList)
+        updatelinkList(e,id)
+        setNewItem("")
     }
 
     const removelinkList = async (e,list) => {
@@ -138,8 +151,12 @@ export default function LinksPage() {
         console.log(`deleting link List: ${list.description}`)
     }
 
+    useEffect(()=> {
+        setlinkList({...linkList,id: currentId, items: itemsList})
+    },[itemsList])
+
     useEffect(() => {
-        setlinkList({...linkList, description: listName})
+        setlinkList({...linkList,id: currentId, description: listName})
     },[listName])
     
     useEffect(() => {
@@ -149,7 +166,7 @@ export default function LinksPage() {
     return (
         <Grid container spacing ={3} className={classes.root}>
             <div>
-                <Button aria-describedby={newList} variant="contained" color="primary" onClick={handleNewListClick}>
+                <Button className={classes.newListButton} aria-describedby={newList} variant="contained" color="primary" onClick={handleNewListClick}>
                     Add List
                 </Button>
                 <Popover
@@ -164,7 +181,7 @@ export default function LinksPage() {
                     }}
                     className={classes.popover}
                 >
-                    <form  noValidate autoComplete="off" onSubmit={addtolinkLists}>
+                    <form noValidate autoComplete="off" onSubmit={addtolinkLists}>
                         <h4 className={classes.popoverTitle}> Add List</h4>          
                         <TextField value={listName} id="basic" label="List Name" variant="outlined" onChange={(e) => {setlistName(e.target.value)}} className={classes.popoverText}/>
                         <Button type="submit" className={classes.popoverButton}>Add</Button>
@@ -176,21 +193,40 @@ export default function LinksPage() {
                         <div >
                             <Card className={classes.card} variant="outlined">
                                     <CardHeader title={list.description} className={classes.cardHeader}/>
-                                        <form onSubmit={e=> changingListName(e,list,list.id)}>
-                                            <TextField value={listName} id="basic" label="update name" variant="outlined" onChange={(e) => {setlistName(e.target.value)}} className={classes.popoverText}/>
+                                        <form onSubmit={e=> {
+                                            updatelinkList(e,list.id)}}>
+                                            <TextField value={listName} id="basic" label="update name" variant="outlined" onChange={(e) => {setCurrentId(list.id)
+                                            setlistName(e.target.value)}} className={classes.popoverText}/>
                                             <Button type="submit" className={classes.popoverButton}>Update</Button>
                                         </form>
                                 <CardContent>
                                     <div className={classes.pos} color="textSecondary">                
-                                        <form noValidate autoComplete="off" > 
-                                            <TextField id="standard-basic" label="Links"/>
-                                            <IconButton aria-label="delete" className={classes.margin}>
-                                                <DeleteIcon />
-                                            </IconButton>
-                                        </form>
-                                        <div>
+                                        {list.items.map(item => {
+                                            return (
+                                                <Typography>
+                                                    {item}
+                                                    <IconButton aria-label="delete" className={classes.margin}>
+                                                        <DeleteIcon />
+                                                    </IconButton>
+                                                </Typography>
+                                            )
+                                        })}
+                                        {
+                                            newItemAnchor &&
+                                            <div className={classes.newItemInput}>
+                                                <form noValidate autoComplete="off" onSubmit={(e)=> 
+                                                {setCurrentId(list.id)
+                                                    updateItems(e,list,list.id)}}>
+                                                    <TextField id="standard-basic" label="Links" value={newItem} onChange={(e)=>{setNewItem(e.target.value)}}/>
+                                                    <IconButton aria-label="delete" className={classes.margin}>
+                                                        <DeleteIcon />
+                                                    </IconButton>
+                                                </form>
+                                            </div>
+                                        }
+                                        <IconButton onClick={(e)=>{setnewItemAnchor(true)}}>
                                             <AddCircleRoundedIcon style={{fill: "#4054b4"}}/>                
-                                        </div>
+                                        </IconButton> 
                                     </div>
                                     <Button variant="contained" color="secondary" onClick={(e) => {removelinkList(e,list)}}>
                                         Delete
